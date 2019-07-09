9b71c75000b0a60a1a28545d9a8c1edd0485d8e2
gregoriB <brandon.gregori@gmail.com>
2019-07-09 14:05:08 -0700

    small change to home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -8,8 +8,8 @@ const Home = () => {
                 <section className='home-container'>
                     <h1>Hello!  I'm Brandon.</h1>
                     <p>
-                        I love web development.  Solving problems, challenging myself, and being creative are things I enjoy, and web development lets me do all of that at once.
-                    <a className='resume' href='https://novoresume.com/a/brandon.gregori.1' target='_blank' rel="noopener noreferrer">Resume</a>
+                        Solving problems, challenging myself, and being creative are things I enjoy quite a bit.  That's why I love web development.
+                        <a className='resume' href='https://novoresume.com/a/brandon.gregori.1' target='_blank' rel="noopener noreferrer">Resume</a>
                     </p>
                 </section>
             </div>
