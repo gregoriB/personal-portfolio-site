c8646aebbf44a861be7b4513016dec9431633651
gregoriB <brandon.gregori@gmail.com>
2019-07-08 20:49:42 -0700

    added nodemailer for contact form email forwarding

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,5 +1,6 @@
+const nodemailer = require("nodemailer");
+
 exports.handler = (event, _, callback) => {
-  console.log(event.body)
   callback(null, {
     statusCode: 200,
     headers: { 
@@ -7,6 +8,31 @@ exports.handler = (event, _, callback) => {
       "Access-Control-Allow-Methods": "POST",
       'Access-Control-Allow-Headers': 'Content-Type', 
     },
-    body: JSON.stringify(event.body)
+    body: JSON.stringify(event.body),
+    main: (async function(){
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_ADDRESS,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      });
+      const { email, text, name } = JSON.parse(event.body)
+
+      const info = await transporter.sendMail({
+        from: email,
+        to: "brandon.gregori@gmail.com, gregori.email.forwarder@gmail.com",
+        subject: `Email from ${name}, email: ${email}`,
+        text: email + text,
+        html: `<a href="mailto: ${email}">${email}</a><p>${text}</p>`
+      });
+      console.log("NEW EMAIL SENT")
+      console.log(info.messageId);
+      console.log("name: " + name);
+      console.log("email: " + email);
+      console.log("message: " + text);
+    })().catch(console.error)
   });
 }
