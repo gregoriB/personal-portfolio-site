771a240c60fd8247f8794f8f7cc6cf5a0bfffc41
gregoriB <brandon.gregori@gmail.com>
2019-07-20 23:44:33 -0700

    changed resume link to local pdf

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -10,7 +10,7 @@ const Home = () => {
                     <p>
                         Solving problems, challenging myself, and being creative are things I enjoy quite a bit.  That's why I love web development.
                     </p>
-                    <a className='resume' href='https://drive.google.com/file/d/109L-Mmjov8tnW3v0T1lq5Sk7S7NoKe4U/view?usp=sharing' target='_blank' rel="noopener noreferrer">View my Résumé</a>
+                    <a className='resume' href={process.env.PUBLIC_URL + '/resume.pdf'} target='_blank' rel="noopener noreferrer">View my Résumé</a>
                 </section>
             </div>
             <div className='decorative-1' />
