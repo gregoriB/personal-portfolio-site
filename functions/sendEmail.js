3e0b892657c3332b5646045a0d35616de358de09
gregorib <brandon.gregori@gmail.com>
2019-09-18 19:30:57 -0700

    updated nodemailer config

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,34 +1,37 @@
 const nodemailer = require("nodemailer");
 
 exports.handler = (event, _, callback) => {
-  callback(null, {
-    statusCode: 200,
-    headers: { 
-      'Access-Control-Allow-Origin': '*',
-      "Access-Control-Allow-Methods": "POST",
-      'Access-Control-Allow-Headers': 'Content-Type', 
-    },
-    body: JSON.stringify(event.body),
-    mailer: (async function(){
-      const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.EMAIL_ADDRESS,
-          pass: process.env.EMAIL_PASSWORD
-        }
-      });
-      const { email, text, name } = JSON.parse(event.body);
-      const info = await transporter.sendMail({
-        from: email,
-        to: "brandon@brandon-gregori.com",
-        replyTo: email,
-        subject: `${name} from ${email} has reached out to you`,
-        text: `${name}, ${text}`,
-        html: ` <p style='color:black; opacity:1; visibility:visible; max-width:600px; display:block;'>${text}</p>`
-      });
-      console.log(`
+    callback(null, {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "Content-Type"
+        },
+        body: JSON.stringify(event.body),
+        mailer: (async function() {
+            const transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 465,
+                secure: true,
+                tls: {
+                    rejectUnauthorized: false
+                },
+                auth: {
+                    user: process.env.EMAIL_ADDRESS,
+                    pass: process.env.EMAIL_PASSWORD
+                }
+            });
+            const { email, text, name } = JSON.parse(event.body);
+            const info = await transporter.sendMail({
+                from: email,
+                to: "brandon@brandon-gregori.com",
+                replyTo: email,
+                subject: `${name} from ${email} has reached out to you`,
+                text: `${name}, ${text}`,
+                html: ` <p style='color:black; opacity:1; visibility:visible; max-width:600px; display:block;'>${text}</p>`
+            });
+            console.log(`
         '~~~~~~~~~~~~~~~~~~~~~~~~~~* --NEW EMAIL SENT-- *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
         -------id: ${info.messageId}
@@ -38,6 +41,6 @@ exports.handler = (event, _, callback) => {
 
         .~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~.
       `);
-    })().catch(console.error)
-  });
-}
+        })().catch(console.error)
+    });
+};
